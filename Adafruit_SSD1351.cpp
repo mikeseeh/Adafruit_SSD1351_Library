ce179403a08d7614df656faa97f2b75ed031b492
Mike Heininger <mike@heininger.at>
2017-04-17 17:04:56 +0200

    clean code

--- Adafruit_SSD1351.cpp
+++ Adafruit_SSD1351.cpp
@@ -1,18 +1,18 @@
-/*************************************************** 
-  This is a library for the 1.5" & 1.27" 16-bit Color OLEDs 
+/***************************************************
+  This is a library for the 1.5" & 1.27" 16-bit Color OLEDs
   with SSD1331 driver chip
 
   Pick one up today in the adafruit shop!
   ------> http://www.adafruit.com/products/1431
   ------> http://www.adafruit.com/products/1673
 
-  These displays use SPI to communicate, 4 or 5 pins are required to  
+  These displays use SPI to communicate, 4 or 5 pins are required to
   interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
   products from Adafruit!
 
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  Written by Limor Fried/Ladyada for Adafruit Industries.
   BSD license, all text above must be included in any redistribution
  ****************************************************/
 
@@ -32,7 +32,7 @@
 
 inline void Adafruit_SSD1351::spiwrite(uint8_t c) {
     //Serial.println(c, HEX);
-    
+
     if (!_sid) {
         SPI.transfer(c);
 		// might be able to make this even faster but
@@ -40,19 +40,19 @@ inline void Adafruit_SSD1351::spiwrite(uint8_t c) {
 		//delayMicroseconds(1);
         return;
     }
- 
+
 	//Software SPI, MSB first
 	for (uint8_t bit = 0; bit < 8; bit++)  {
 		//PIN_MAP[_sclk].gpio_peripheral->BRR = PIN_MAP[_sclk].gpio_pin; // Clock Low
 		pinResetFast(_sclk);
-		
+
 		if (c & (1 << (7-bit)))		// walk down mask from bit 7 to bit 0
 			//PIN_MAP[_sid].gpio_peripheral->BSRR = PIN_MAP[_sid].gpio_pin; // Data High
 			pinSetFast(_sid);
 		else
 			//PIN_MAP[_sid].gpio_peripheral->BRR = PIN_MAP[_sid].gpio_pin; // Data Low
 			pinResetFast(_sid);
-			
+
 		//PIN_MAP[_sclk].gpio_peripheral->BSRR = PIN_MAP[_sclk].gpio_pin; // Clock High
 		pinSetFast(_sclk);
 	}
@@ -64,10 +64,10 @@ void Adafruit_SSD1351::writeCommand(uint8_t c) {
 
     digitalWriteFast(_rs, LOW);
     digitalWriteFast(_cs, LOW);
-    
+
     //Serial.print("C ");
     spiwrite(c);
-    
+
     digitalWriteFast(_cs, HIGH);
 }
 
@@ -75,18 +75,18 @@ void Adafruit_SSD1351::writeCommand(uint8_t c) {
 void Adafruit_SSD1351::writeData(uint8_t c) {
     digitalWriteFast(_rs, HIGH);
     digitalWriteFast(_cs, LOW);
-    
+
 //    Serial.print("D ");
     spiwrite(c);
-    
+
     digitalWriteFast(_cs, HIGH);
-} 
+}
 
 /***********************************/
 
 void Adafruit_SSD1351::goTo(int x, int y) {
   if ((x >= SSD1351WIDTH) || (y >= SSD1351HEIGHT)) return;
-  
+
   // set x and y coordinate
   writeCommand(SSD1351_CMD_SETCOLUMN);
   writeData(x);
@@ -96,7 +96,7 @@ void Adafruit_SSD1351::goTo(int x, int y) {
   writeData(y);
   writeData(SSD1351HEIGHT-1);
 
-  writeCommand(SSD1351_CMD_WRITERAM);  
+  writeCommand(SSD1351_CMD_WRITERAM);
 }
 
 uint16_t Adafruit_SSD1351::Color565(uint8_t r, uint8_t g, uint8_t b) {
@@ -131,7 +131,7 @@ void Adafruit_SSD1351::rawFillRect(uint16_t x, uint16_t y, uint16_t w, uint16_t 
   {
     w = SSD1351WIDTH - x - 1;
   }
-  
+
   /*
   Serial.print(x); Serial.print(", ");
   Serial.print(y); Serial.print(", ");
@@ -147,7 +147,7 @@ void Adafruit_SSD1351::rawFillRect(uint16_t x, uint16_t y, uint16_t w, uint16_t 
   writeData(y);
   writeData(y+h-1);
   // fill!
-  writeCommand(SSD1351_CMD_WRITERAM);  
+  writeCommand(SSD1351_CMD_WRITERAM);
 
   for (uint16_t i=0; i < w*h; i++) {
     writeData(fillcolor >> 8);
@@ -206,7 +206,7 @@ void Adafruit_SSD1351::rawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t co
   writeData(y);
   writeData(y);
   // fill!
-  writeCommand(SSD1351_CMD_WRITERAM);  
+  writeCommand(SSD1351_CMD_WRITERAM);
 
   for (uint16_t i=0; i < w; i++) {
     writeData(color >> 8);
@@ -236,7 +236,7 @@ void Adafruit_SSD1351::rawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t co
   writeData(y);
   writeData(y+h-1);
   // fill!
-  writeCommand(SSD1351_CMD_WRITERAM);  
+  writeCommand(SSD1351_CMD_WRITERAM);
 
   for (uint16_t i=0; i < h; i++) {
     writeData(color >> 8);
@@ -316,21 +316,21 @@ void Adafruit_SSD1351::drawPixel(int16_t x, int16_t y, uint16_t color)
   if ((x < 0) || (y < 0)) return;
 
   goTo(x, y);
-  
+
   // setup for data
   digitalWriteFast(_rs, HIGH);
   digitalWriteFast(_cs, LOW);
-  
-  spiwrite(color >> 8);    
+
+  spiwrite(color >> 8);
   spiwrite(color);
-  
-  digitalWriteFast(_cs, HIGH);  
+
+  digitalWriteFast(_cs, HIGH);
 }
 
 void Adafruit_SSD1351::begin(void) {
     // set pin directions
     pinMode(_rs, OUTPUT);
-    
+
     if (_sclk) {
         pinMode(_sclk, OUTPUT);
         pinMode(_sid, OUTPUT);
@@ -340,7 +340,7 @@ void Adafruit_SSD1351::begin(void) {
 		SPI.setClockDivider(SPI_CLOCK_DIV8);	// 72MHz / 8 = 9Mhz
         SPI.setDataMode(SPI_MODE3);
     }
-	
+
     // Toggle RST low to reset; CS low so it'll listen to us
     pinMode(_cs, OUTPUT);
     digitalWrite(_cs, LOW);
@@ -357,7 +357,7 @@ void Adafruit_SSD1351::begin(void) {
 
     // Initialization Sequence
     writeCommand(SSD1351_CMD_COMMANDLOCK);  // set command lock
-    writeData(0x12);  
+    writeData(0x12);
     writeCommand(SSD1351_CMD_COMMANDLOCK);  // set command lock
     writeData(0xB1);
 
@@ -365,13 +365,13 @@ void Adafruit_SSD1351::begin(void) {
 
     writeCommand(SSD1351_CMD_CLOCKDIV);  		// 0xB3
     writeCommand(0xF1);  						// 7:4 = Oscillator Frequency, 3:0 = CLK Div Ratio (A[3:0]+1 = 1..16)
-    
+
     writeCommand(SSD1351_CMD_MUXRATIO);
     writeData(127);
-    
+
     writeCommand(SSD1351_CMD_SETREMAP);
     writeData(0x74);
-  
+
     writeCommand(SSD1351_CMD_SETCOLUMN);
     writeData(0x00);
     writeData(0x7F);
@@ -392,7 +392,7 @@ void Adafruit_SSD1351::begin(void) {
 
     writeCommand(SSD1351_CMD_SETGPIO);
     writeData(0x00);
-    
+
     writeCommand(SSD1351_CMD_FUNCTIONSELECT);
     writeData(0x01); // internal (diode drop)
     //writeData(0x01); // external bias
@@ -402,7 +402,7 @@ void Adafruit_SSD1351::begin(void) {
 
     writeCommand(SSD1351_CMD_PRECHARGE);  		// 0xB1
     writeCommand(0x32);
- 
+
     writeCommand(SSD1351_CMD_VCOMH);  			// 0xBE
     writeCommand(0x05);
 
@@ -420,11 +420,11 @@ void Adafruit_SSD1351::begin(void) {
     writeData(0xA0);
     writeData(0xB5);
     writeData(0x55);
-    
+
     writeCommand(SSD1351_CMD_PRECHARGE2);
     writeData(0x01);
-    
-    writeCommand(SSD1351_CMD_DISPLAYON);		//--turn on oled panel    
+
+    writeCommand(SSD1351_CMD_DISPLAYON);		//--turn on oled panel
 }
 
 void  Adafruit_SSD1351::invert(boolean v) {
@@ -454,38 +454,3 @@ Adafruit_SSD1351::Adafruit_SSD1351(uint8_t cs, uint8_t rs,  uint8_t rst) : Adafr
     _sclk = 0;
     _rst = rst;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
